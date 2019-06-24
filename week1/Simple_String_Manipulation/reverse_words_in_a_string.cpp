f83796f48b1e48e8fd383fe43175539081ff275e
Susana Ortega <sorteg@umich.edu>
2019-06-24 00:45:54 -0500

    solved

---
+++ week1/Simple_String_Manipulation/reverse_words_in_a_string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string temp;
+        string ans;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ' '){
+                reverse(temp.begin(), temp.end());
+                ans+=temp;
+                ans.push_back(' ');
+                temp.clear();
+            }
+            else{
+                
+                temp.push_back(s[i]);
+            }
+        }
+        reverse(temp.begin(), temp.end());
+        ans+=temp;
+        return ans;
+    }
+};
