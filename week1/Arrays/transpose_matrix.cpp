c191180e118c7fcb367df645f79461535700a471
Susana Ortega <sorteg@umich.edu>
2019-06-23 23:08:34 -0500

    shows different ways to resize a vector

--- week1/Arrays/transpose_matrix.cpp
+++ week1/Arrays/transpose_matrix.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& A) {
-        vector<vector<int>> B;
-        B.resize(A[0].size());
+        vector<vector<int>> B(A[0].size(), vector<int>(A.size(), 0));
         
-        for(int i = 0; i < A[0].size(); i++){
-            B[i].resize(A.size());
-        }
+        //         vector<vector<int>> B;
+        //         B.resize(A[0].size());
+        //         for(int i = 0; i < A[0].size(); i++){
+        //             B[i].resize(A.size());
+        //         }
         
         for(int row = 0; row < A[0].size(); row++){
             for(int col = 0; col < A.size(); col++){
