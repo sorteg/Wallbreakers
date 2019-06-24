992ca8e1c7c978d8ca9cb3b14946ba75932cbed3
Susana Ortega <sorteg@umich.edu>
2019-06-23 23:02:37 -0500

    solved

---
+++ week1/Arrays/transpose_matrix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        vector<vector<int>> B;
+        B.resize(A[0].size());
+        
+        for(int i = 0; i < A[0].size(); i++){
+            B[i].resize(A.size());
+        }
+        
+        for(int row = 0; row < A[0].size(); row++){
+            for(int col = 0; col < A.size(); col++){
+                B[row][col] = A[col][row];
+            }
+        }
+        return B;
+    }
+};
